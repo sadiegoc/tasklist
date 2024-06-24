853f209aa1cb3e85ddd9a7b84444fb6779378375
Diego Sá <diego.sa1687@hotmail.com>
2024-06-24 13:47:58 -0300

    done

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Tasklist</h1>
+    <TaskList/>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+  name: 'App',
+  components: { TaskList }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'Roboto';
+  src: url(./assets/fonts/Roboto-Regular.ttf);
+}
+
+body {
+  margin: 0; padding: 0;
+  background-color: #151515;
+}
+
+#app {
+    margin: 60px auto; padding: 0;
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    display: flex; flex-direction: column;
+}
+
+h1 {
+  color: white;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="tasks">
+        <table>
+            <thead>
+                <tr>
+                    <td colspan="3">
+                        <input type="text" placeholder="New task" v-model="name">
+                        <button v-on:click="addTask">+</button>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="task in tasklist" :key="task.id">
+                    <td><input type="checkbox" v-model="task.checked" v-on:click="checkTask(task)"></td>
+                    <td>{{ task.name }}</td>
+                    <td><img class="trash" src="../assets/imgs/trash.png" alt="Delete" v-on:click="removeTask(task.id)"></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: "TaskList",
+    data () {
+        return {
+            name: "",
+            checked: false,
+            tasklist: []
+        }
+    },
+    methods: {
+        async loadData () {
+            const response = await axios.get('http://localhost:3000/tasks/');
+            this.tasklist = response.data;
+        },
+        async addTask () {
+            if (this.name) {
+                const response = await axios.post('http://localhost:3000/tasks/', { name: this.name, checked: false });
+                if (response.status == 201) {
+                    this.loadData();
+                    this.name = "";
+                }
+            }
+        },
+        async removeTask (id) {
+            const response = await axios.delete('http://localhost:3000/tasks/'+id);
+            if (response.status == 200)
+                this.loadData();
+        },
+        async checkTask (task) {
+            const response = await axios.put('http://localhost:3000/tasks/'+task.id, { name: task.name, checked: !task.checked });
+            if (response.status == 200)
+                this.loadData();
+        }
+    },
+    mounted () {
+        this.loadData();
+    }
+}
+</script>
+
+<style>
+.tasks {
+    margin: auto; padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+}
+
+.tasks table {
+    margin: 0; padding: 0;
+    border-collapse: separate;
+}
+
+.tasks tbody {
+    margin-top: 20px;
+}
+
+.tasks table td {
+    width: fit-content; height: 40px;
+    text-align: center; vertical-align: middle;
+}
+
+.tasks input[type="checkbox"] {
+    -webkit-appearance: none; appearance: none; margin: 0;
+    font: inherit; color: currentColor;
+    width: 1.15em; height: 1.15em;
+    border: 1px solid currentColor;
+    border-radius: 50%; cursor: pointer;
+}
+
+.tasks input[type="checkbox"]:checked {
+    border-color: aqua; background-color: aqua;
+}
+
+.tasks thead td input {
+    width: 300px; height: 100%;
+    border-radius: 30px; border: none transparent;
+    padding-left: 20px; margin: auto;
+    background-color: #efefef;
+    font-size: .9rem; outline: none;
+}
+
+.tasks thead td button {
+    width: 40px; height: 100%;
+    margin-left: 20px; padding: 0;
+    border-radius: 50%; border: none transparent;
+    background-color: aquamarine;
+    cursor: pointer; font-size: 1.4rem;
+}
+
+.tasks thead td button:hover {
+    background-color: lightgreen;
+}
+
+.trash {
+    padding: 0; margin: 0;
+    cursor: pointer;
+}
+</style>
